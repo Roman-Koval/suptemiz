5d1e72af4872760fe563afcbbe81bb023e298d1c
Roman Koval <77807168+Roman-Koval@users.noreply.github.com>
2026-05-08 00:34:29 +0300

    Set Firebase configuration values
    Updated Firebase configuration with actual credentials.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
     // ← ВСТАВЬ СВОЙ FIREBASE CONFIG
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyDcsvkcECnkcIvJxa6FRpUurgIUgYwW4qg",
+  authDomain: "suptemiz.firebaseapp.com",
+  databaseURL: "https://suptemiz-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "suptemiz",
+  storageBucket: "suptemiz.firebasestorage.app",
+  messagingSenderId: "399676923890",
+  appId: "1:399676923890:web:44657bbb2870a7d76cd56b"
 };
 
 firebase.initializeApp(firebaseConfig);
